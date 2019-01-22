fe9fb19901dc060aa5d3605d106a82b496fa0bfa
Bhuvaneshvar nath Srivastava <bhuvaneshvar8@gmail.com>
2019-01-22 13:49:51 +0530

    second

---
+++ 05WebBasics/myScript.js
@@ -0,0 +1,24 @@
+//alert("File is attached")
+
+// console.log(document.documentURI);
+// console.log(document.title);
+// console.log(document.title);
+//document.title = "I got change" // chnaging title 
+
+
+// getElement by Id,class,name are options
+
+
+ //console.log(document.getElementById("idOne"));
+ //console.log(document.getElementsByClassName("classOne"));
+
+//  const myElement = document.querySelectorAll(".classOne"); // or querySelector -> retuns first tag
+//  console.log(myElement[0]);
+
+ const myPElment = document.querySelectorAll("p");
+//myPElment.textContent ="Hello i am now changed";
+// myPElment.forEach(element => {
+//     element.textContent = "I am changed by js"
+// });
+ 
+ 
